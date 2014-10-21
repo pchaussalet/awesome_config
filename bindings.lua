0a1e7d159aac14a9a78c01f3efcc6b379639be9c
Pierre Chaussalet <pchaussalet@gmail.com>
2014-10-21 10:37:48 +0200

    Restore awesome menu

--- bindings.lua
+++ bindings.lua
@@ -5,6 +5,24 @@ function print_output(command)
   print(text)
 end
 
+-- {{{ Menu
+-- Create a laucher widget and a main menu
+myawesomemenu = {
+   { "manual", terminal .. " -e man awesome" },
+   { "edit config", editor_cmd .. " " .. awful.util.getdir("config") .. "/rc.lua" },
+   { "restart", awesome.restart },
+   { "quit", awesome.quit }
+}
+
+mymainmenu = awful.menu({ 
+  items = {
+    { "awesome", myawesomemenu, beautiful.awesome_icon },
+--                                    { "Debian", debian.menu.Debian_menu.Debian },
+    { "open terminal", terminal }
+  }
+})
+
+
 -- {{{ Key bindings
 globalkeys = awful.util.table.join(
     awful.key({ modkey,           }, "p", function() print_output("xrandr") end),
